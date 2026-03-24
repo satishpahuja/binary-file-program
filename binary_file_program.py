dff41bad3ee46128be1489ed61d64c19df01b0be
satishpahuja <patriotpahuja@gmail.com>
2026-03-24 13:04:37 +0530

    Update binary_file_program.py

--- binary_file_program.py
+++ binary_file_program.py
@@ -31,5 +31,5 @@ def SearchDestn(D):
 #binary fine mein 2 type ke functions aate hai ek write krne ke liye aur ek read krne ke liye write krne ke liye dump function use krte hai aur read krne ke liye load function use krte hain
 #binary file mein upar Create function write krne ke liye hai jabki SearchDestn read krne ke liye hai
 #poora program ke pattern ko as it is samjho bas apko functions ke name, kya input lena hai user se, file ke name dhyan se dekhna hai question mein wahi use karne hai baki poora program as it is learn kr lo
-#line number 21 mein condition change ho skti hai read karte time like hamein Destination match karna hai ki kis record ka destination D hai aur destination hamare record mein third index par hai then woh match karna hai yeh thoda bas record ka index dhyan se dekhna hai apne condition ke time otherwise poora program easy hai
+#line number 22 mein condition change ho skti hai read karte time like hamein Destination match karna hai ki kis record ka destination D hai aur destination hamare record mein third index par hai then woh match karna hai yeh thoda bas record ka index dhyan se dekhna hai apne condition ke time otherwise poora program easy hai
 #binary file program mein pickle module import karna na bhule
